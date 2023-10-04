bbcd585b71d298e4306765920f2e45ff6711f66a
Th3VaultDweller <aztec242@yandex.ru>
2023-10-04 23:24:37 +0300

    Added ChatDataSet

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import json
 
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
 
 from nltk_utils import bag_of_words, stem, tokenize
 
@@ -42,3 +45,15 @@ for (pattern_sentence, tag) in xy:
 
 x_train = np.array(x_train)
 y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples=len(x_train)
+        self.x_data= x_train
+        self.y_data= y_train
+    
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
