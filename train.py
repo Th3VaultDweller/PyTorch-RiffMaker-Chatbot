be6300390cda651d8f011e4f498350755260727e
Th3VaultDweller <aztec242@yandex.ru>
2023-10-04 22:57:51 +0300

    Added training data

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import json
-
+import numpy as np
 from nltk_utils import bag_of_words, stem, tokenize
 
 with open("intents.json", "r") as f:
@@ -21,6 +21,23 @@ for intent in intents["intents"]:
 ignore_words = ["?", "!", "*", "'", ".", ","]
 all_words = [stem(w) for w in all_words if w not in ignore_words]  # stemming
 print(all_words)
-all_words = sorted(set(all_words))
+
+print(f"\n")
+
+all_words = sorted(set(all_words))  # sorted function will return a list
 tags = sorted(set(tags))
 print(tags)
+
+# training data
+x_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label) #CrossEntropyLoss
+
+x_train=np.array(x_train)
+y_train=np.array(y_train)
+
