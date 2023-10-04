ccc5c3885dd5ef8a49dcc559eeb5d7b6720c5125
Th3VaultDweller <aztec242@yandex.ru>
2023-10-04 19:39:56 +0300

    Added tokenize and stemming

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,10 +1,17 @@
 import nltk
 
-def tokenize(sentence):
-    pass
 
-def stemming (word):
-    pass
+def tokenize(sentence):
+    nltk.download("punkt")
+    from nltk.stem.porter import PorterStemmer
+
+    stemmer = PorterStemmer()
+    return nltk.word_tokenize(sentence)
+
+
+def stemming(word):
+    return stemmer.stem(word.lower)
+
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
