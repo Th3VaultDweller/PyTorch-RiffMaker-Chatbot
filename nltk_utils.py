80c60f675b60c75d9d113a4a6bcff92a9e58e0b6
Th3VaultDweller <aztec242@yandex.ru>
2023-10-04 22:09:27 +0300

    Minor fixes and tests

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,16 +1,23 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+# nltk.download("punkt")
+stemmer = PorterStemmer()
+
 
 def tokenize(sentence):
-    nltk.download("punkt")
-    stemmer = PorterStemmer()
     return nltk.word_tokenize(sentence)
 
 
-def stemming(word):
-    return stemmer.stem(word.lower)
+def stem(word):
+    return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
+
+# Testing the tokenize function
+a = "I want to learn a new bass riff"
+print(a)
+a = tokenize(a)
+print(a)
